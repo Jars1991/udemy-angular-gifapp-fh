947ab277b1cdb838b1983c2faff17ad8eeb2490e
Jars1991 <jassael1991@gmail.com>
2021-04-29 15:24:36 -0500

    add http client for http requests

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -7,9 +8,15 @@ export class GifsService {
 
   private _searchHistory: string[] = [];
 
+  private _API_KEY = 'qg8MHYLy7gB3gwMhNlJI76T6rMQEWNiL';
+  private _API_URL = 'https://api.giphy.com/v1/gifs/search';
+  private _PAGINATION_LIMIT = 10;
+
   get searchHistory() {
     return [...this._searchHistory];
   }
+
+  constructor(private _HttpClient: HttpClient) { }
 
   search(query: string) {
     query = query.trim().toLowerCase();
@@ -19,7 +26,13 @@ export class GifsService {
       this._searchHistory = this._searchHistory.slice(0, 10);
       console.log(this._searchHistory);
     };
+
+    const _params = {q: query, api_key: this._API_KEY, limit: `${this._PAGINATION_LIMIT}`};
+
+    this._HttpClient.get(this._API_URL, {params: _params})
+      .subscribe((response: any) => {
+        console.log(response.data);
+      })
   }
 
-  constructor() { }
 }
